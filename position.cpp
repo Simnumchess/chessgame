3bc1e6b9d2679a70bb28f626ed6a95bc45537855
kaeria <inesguo93@gmail.com>
2015-02-20 10:59:52 +0100

    Update position.cpp

--- position.cpp
+++ position.cpp
@@ -1,18 +1,52 @@
 #include "position.h"
+#include "echiquier.h"
+#include "piece.h"
 
 using namespace std;
 
-Joueur Position::getJoueur()
+Joueur position::getJoueur()
 {
-    return Joueur_pos;
+    if()
+    return humain;
+    if()
+    return ordinadeur;
 }
 
-Position::~Position()
+position::~position()
 {
     if((pos_soeur!=NULL)||(pos_fille!=NULL))
     delete []pos_fille;
     delete []pos_soeur;
 }
+position::val_pos_ordinateur()
+{
+    for(int l=0; l<8; l++)
+    {
+        for(int c=0;c<8;c++)
+        if(!(bool casevide(int l,int c)))
+        {
+            if(ehcectab[l][c].couleur==noir)
+            val_pos_ordinateur=val_pos_ordinateur+echectab[l][c].getvaleurpiece();
+
+        }
+    }
+    return val_pos_ordinateur;
+}
+position::val_pos_humain()
+{
+    for(int l=0; l<8; l++)
+    {
+        for(int c=0;c<8;c++)
+        if(!(bool casevide(int l,int c)))
+        {
+            if(ehcectab[l][c].couleur==blanc)
+            val_pos_humain=val_pos_humain+echectab[l][c].getvaleurpiece();
+
+        }
+    }
+    return val_pos_humain;
+}
+
 
 //classe abstraite position
 // classes filles position pour ttt et pour echecs
