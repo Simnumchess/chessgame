1db7da6773dc585ee1d017d919c2579cc6e6a84e
Marcow12 <marcantoinew12@gmail.com>
2015-02-20 11:46:08 +0100

    Update coup.cpp

--- coup.cpp
+++ coup.cpp
@@ -1,2 +1,29 @@
 #include "coup.h"
+#include <stdlib.h>
+#include <piece.h>
 
+    coup::coup()
+    {
+    }
+    couleur col;
+    piece piece_jouee;
+    position pos_ancienne;
+    position pos_nouvelle;
+    bool prise_de_piece;
+    enum coup::coup_speciaux
+    {
+        petit_roque;
+        promotion;
+        prise_en_passant;
+
+    }
+
+
+    coup::~coup()
+    {
+
+    }
+    void coup::print()
+    {
+
+    }
