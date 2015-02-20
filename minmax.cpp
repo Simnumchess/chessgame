e62f2d0feaed7e72fa9a47e90bda97919db59330
Ilana Lascar <ilana_lascar@hotmail.com>
2015-02-20 10:59:22 +0100

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -1,17 +1,30 @@
-#include <min-max.h>
+#include "minmax.h"
+#include "position.h"
 
 int generation
 
 
 
-int minmax(Position position, Coup coup){
+int minmax(Position position){
 
-if (position.categorie_position==position_terminale) return position.valeur_position;
-else if (position.categorie_position==position_joueur) max()
-else return min();
+if (position.getJoueur==fin_partie) return position.val_pos;
+
+else if (position.getJoueur==humain) max();
+
+else if (position.getJoueur==ordinateur) min();
+
+else return 0;
+
 }
+
+
+
+int min();
+
+int max();
 
 // variante alpha beta
 int alpha_beta(int profondeur,int alpha, int beta){
 
 }
+
