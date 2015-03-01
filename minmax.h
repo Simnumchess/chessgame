c52313f816f9a88f859cd7c8cebb21d4b73ffb35
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-01 23:26:55 +0100

    Update minmax.h

--- minmax.h
+++ minmax.h
@@ -3,8 +3,8 @@
 
 //minmax classique
 int minmax(Position &P, int profondeur);
-int min(Position &P, int profondeur);//min à l'interieur du minmax
-int max(Position &P, int profondeur);//max à l'interieur du minmax
+int minmax_min(Position &P, int profondeur);//min à l'interieur du minmax
+int minmax_max(Position &P, int profondeur);//max à l'interieur du minmax
 
 //variante alpha-beta
 int alpha_beta(Position &position, int profondeur,int alpha, int beta); 
