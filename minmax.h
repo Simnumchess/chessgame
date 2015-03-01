c878a67dd382471457418c5e2de95f00914a2d9b
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-01 21:17:53 +0100

    Update minmax.h

--- minmax.h
+++ minmax.h
@@ -1,8 +1,8 @@
 #ifndef MINMAX_H_INCLUDED
 #define MINMAX_H_INCLUDED
 
-int minmax(int profondeur); //minmax classique
+int minmax(position &P, int profondeur); //minmax classique
 
-int alpha_beta(int profondeur,int alpha, int beta); //variante alpha-beta
+int alpha_beta(position &P, int profondeur,int alpha, int beta); //variante alpha-beta
 
 #endif // MINMAX_H_INCLUDED
