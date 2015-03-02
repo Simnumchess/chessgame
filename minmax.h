9730ff99f9de4fd425d5217e2d3e5b96f1886484
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-02 10:56:40 +0100

    Update minmax.h

--- minmax.h
+++ minmax.h
@@ -8,7 +8,5 @@ int minmax(Position_echecs &P, int profondeur);
 int minmax_min(Position_echecs &P, int profondeur);//min à l'interieur du minmax
 int minmax_max(Position_echecs &P, int profondeur);//max à l'interieur du minmax
 
-//variante alpha-beta
-int alpha_beta(Position_echecs &P, int profondeur,int alpha, int beta); 
 
 #endif // MINMAX_H_INCLUDED
