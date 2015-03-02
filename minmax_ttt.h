08b2711210772c8ae28fba77aaf72f216946c80c
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-02 09:42:15 +0100

    Update minmax_ttt.h

--- minmax_ttt.h
+++ minmax_ttt.h
@@ -1,1 +1,13 @@
+#ifndef MINMAX_H_INCLUDED
+#define MINMAX_H_INCLUDED
+
+//minmax classique
+int minmax(Position &P, int profondeur);
+int minmax_min(Position &P, int profondeur);//min à l'interieur du minmax
+int minmax_max(Position &P, int profondeur);//max à l'interieur du minmax
+
+//variante alpha-beta
+int alpha_beta(Position &position, int profondeur,int alpha, int beta); 
+
+#endif // MINMAX_H_INCLUDED
 
