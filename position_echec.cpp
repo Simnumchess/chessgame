e8d61cff30a6666e3ff29ee0034d466e206ef764
kaeria <inesguo93@gmail.com>
2015-03-02 10:20:10 +0100

    Update position_echec.cpp

--- position_echec.cpp
+++ position_echec.cpp
@@ -30,4 +30,20 @@ double position_echec::getvaleur(double alpha,double beta) const// la fonction q
     valeur=alpha*(position.val_pos_humain-position.val_pos_ordinateur)+beta*(cont_humain-cont_ordinateur);
     return valeur;
 }
+position_ehec* position_echec::get_pos-suiv() const
+{
+    position_echec* pos_fille=new position_echec[100];
+    if(Jeu_pos==humain)
+    {
+        for(int l=0;l<8;l++)
+        {
+            for(int c=0;c<8;c++)
+            {
+                if((echiquier_ref.echectab[l][c]!=PV)&&(echiquier_ref.ehectab[l][c].col==blanc))
+                
+            }
+        }
+    }
+    
+}
 
