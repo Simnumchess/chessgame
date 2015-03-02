bf9f7627744054f1e3abc18e428ca5c6f50ae222
kaeria <inesguo93@gmail.com>
2015-03-02 10:26:28 +0100

    Update position_echec.cpp

--- position_echec.cpp
+++ position_echec.cpp
@@ -33,17 +33,31 @@ double position_echec::getvaleur(double alpha,double beta) const// la fonction q
 position_ehec* position_echec::get_pos-suiv() const
 {
     position_echec* pos_fille=new position_echec[100];
-    if(Jeu_pos==humain)
+    if(Jeu_pos==humain) //si le humain joue
     {
         for(int l=0;l<8;l++)
         {
             for(int c=0;c<8;c++)
             {
                 if((echiquier_ref.echectab[l][c]!=PV)&&(echiquier_ref.ehectab[l][c].col==blanc))
+                coup possible??
                 
             }
         }
+        return pos_fille;
     }
-    
+    else if(Jeu_pos==ordinateur) //si l'ordinateur joue
+    {
+        for(int l=0;l<8;l++)
+        {
+            for(int c=0;c<8;c++)
+            {
+                if((echiquier_ref.echectab[l][c]!=PV)&&(echiquier_ref.ehectab[l][c].col==noir))
+                coup possible??
+                
+            }
+        }
+        return pos_fille;
+    }
 }
 
