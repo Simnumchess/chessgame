bfdd8aa4a9d15713eebafffcee14abdd5a67b56d
kaeria <inesguo93@gmail.com>
2015-03-03 21:11:09 +0100

    Update piece.h

--- piece.h
+++ piece.h
@@ -12,9 +12,11 @@ class Piece
     void print(); //print d'une piece selon son type
     int placementl; //un deplacement relatif en ligne
     int placementc; //un deplacement relatif en colone
+    int i; //la position d'une piece en ligne
+    int j; //la position d'une piece en colone
     bool deplacement(int l,int c); //regarde si on peut faire un deplacement
     int val; //valeur d'une piece: val_roi=0; val_pion=v; val_reine=9*v; val_tour=5*v; val_fou=3*v; val_cavalier=3*v
-    int getvaleurpiece(int v); //renvoie la valeur d'une piece selon son type
+    int getvaleurpiece(); //renvoie la valeur d'une piece selon son type
     piece(); //constructeur d'une piece
     piece & operator=(const piece &); //operateur=
     ~piece(); //destructeur d'une piece
