a46e7e330a308753db758cf2ec5b46caa880a70f
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-04 12:04:50 +0100

    Update minmax_ttt.h

--- minmax_ttt.h
+++ minmax_ttt.h
@@ -1,10 +1,11 @@
 #ifndef MINMAX_TTT_H_INCLUDED
 #define MINMAX_TTT_H_INCLUDED
+#include "position_ttt.h"
 
 //minmax_ttt classique
-int minmax_ttt(Position_ttt &P, int profondeur);
-int minmax_ttt_min(Position_ttt &P, int profondeur);//min à l'interieur du minmax
-int minmax_ttt_max(Position_ttt &P, int profondeur);//max à l'interieur du minmax
+int minmax_ttt(position_ttt &P, int profondeur);
+int minmax_ttt_min(position_ttt &P, int profondeur);//min à l'interieur du minmax
+int minmax_ttt_max(position_ttt &P, int profondeur);//max à l'interieur du minmax
 
 
 #endif // MINMAX_TTT_H_INCLUDED
