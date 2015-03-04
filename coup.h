7f758dfe11c25adc70c50f73a98a939e79e554e9
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-04 10:10:58 +0100

    Update coup.h

--- coup.h
+++ coup.h
@@ -2,19 +2,19 @@
 #define COUP_H_INCLUDED
 #include "piece.h"
 #include "echiquier.h"
-class Coup
+class coup
 {
     public:
     
-    Couleur col;
-    Piece piece_jouee;
-    Piece piece_mangee;
+    couleur col;
+    piece piece_jouee;
+    piece piece_mangee;
     
     bool coup_possible(echiquier &); //explorer les coups possible en base des placements possibles
-    Coup(); //initialisation d'un coup;
+    coup(); //initialisation d'un coup;
     
-    Position pos_ancienne; //l'ancienne position
-    Position pos_nouvelle; //la nouvelle position
+    position pos_ancienne; //l'ancienne position
+    position pos_nouvelle; //la nouvelle position
     bool prise_de_piece(); //tester s'il y a prise de piece(un est mangee par l'autre)
     enum coup_speciaux; //tout les coup speciaux
     bool coup_special(); //tester si c'est un coup special
