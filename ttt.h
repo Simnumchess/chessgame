37a2fe4b093841cab2ec573b9ef126304da39c2b
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-04 23:07:25 +0100

    Update ttt.h

--- ttt.h
+++ ttt.h
@@ -1,1 +1,24 @@
+#ifndef TTT_H_INCLUDED
+#define TTT_H_INCLUDED
+#include "definition.h"
+#include "piece.h"
+#include "position.h"
+#include "position_ttt.h"
+using namespace std;
+
+class ttt
+{
+    public:
+    int dim;
+    Piece **ttttab; //le tableau 3*3 de pointeurs de pieces
+    bool casevide(int l,int c) //prend un case de coordonee (l,c) et ragarde si la case est vide
+    void print(); //print un ttt
+    echiquier(); //initialisation d'un echiquier
+    ttt(const ttt &); //operateur par copie
+    ttt & operator=(const operator &); //operateur=
+    ~ttt(); //destructeur d'un ttt
+};
+
+
+#endif // TTT_H_INCLUDED
 
