9a4f8df9615f433a5a992e5915b8ce4d2ecf454d
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-04 11:54:00 +0100

    Update and rename position_echec.h to position_echecs.h

--- position_echecs.h
+++ position_echecs.h
@@ -5,7 +5,7 @@
 #include "echiquier.h"
 #include "coup.h"
 
-class position_echec : class position
+class position_echecs : class position
 {
     public:
     coup coup_joue; //
@@ -16,14 +16,14 @@ class position_echec : class position
     position* pos_fille; //un pointeur sur la premiere position fille
     Joueur Jeu_pos; //un indicateur sur le joueur qui doit jouer cette position: {humain;ordinateur}
     echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
-    position_echec maj(); //mise a jour des positions
+    position_echecs maj(); //mise a jour des positions
     double getvaleur() const; //renvoie la valeur d'une position
-    position_ehec* get_pos_suiv(); //constructeur de toutes les positions suivantes a une profondeur donnee
+    position_echecs* get_pos_suiv(); //constructeur de toutes les positions suivantes a une profondeur donnee
     
-    position_echec(); //constructeur d'une position
-    position_echec(const position_echec &) //constructeur par copie
-    position_echec & operator=(const position_echec &) //operateur =
-    ~position_echec(); //destructeur d'une position
+    position_echecs(); //constructeur d'une position
+    position_echecs(const position_echec &) //constructeur par copie
+    position_echecs & operator=(const position_echec &) //operateur =
+    ~position_echecs(); //destructeur d'une position
 };
 void partie(); //definiton d'une partie
 
