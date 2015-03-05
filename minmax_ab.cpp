0e89339e93b1262b4f9f4aa2f7eb291c2acb326d
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-05 15:29:14 +0100

    Update minmax_ab.cpp

--- minmax_ab.cpp
+++ minmax_ab.cpp
@@ -10,10 +10,37 @@ consiste à stopper l'exploration d'une branche qd:
 
 */
 
+/*fonction ALPHABETA(P, alpha, beta) / alpha est toujours inférieur à beta 
+   si P est une feuille alors
+       retourner la valeur de P
+   sinon
+       si P est un nœud Min alors
+           Val = infini
+           pour tout enfant Pi de P faire
+               Val = Min(Val, ALPHABETA(Pi, alpha, beta))                
+               si alpha ≥ Val alors  / coupure alpha /
+                   retourner Val
+               finsi    
+               beta = Min(beta, Val)
+           finpour            
+       sinon
+           Val = -infini
+           pour tout enfant Pi de P faire
+               Val = Max(Val, ALPHABETA(Pi, alpha, beta))                
+               si Val ≥ beta alors / coupure beta /
+                   retourner Val
+               finsi   
+               alpha = Max(alpha, Val)
+           finpour
+       finsi
+   retourner Val
+   finsi
+fin
+*/
 
 //rajouter double alpha, double beta
 
-int minmax_ab(position_echecs &P, int profondeur){
+int minmax_ab(position_echecs &P, int profondeur, double alpha, double beta){
 
 cout<<"variante alpha beta"<<endl;
 
