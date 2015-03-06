ffe48000f286df17f5efa36df30a3bdbdb5eb4db
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-06 16:08:04 +0100

    Update minmax.h

--- minmax.h
+++ minmax.h
@@ -1,1 +1,12 @@
+#ifndef MINMAX_H_INCLUDED
+#define MINMAX_H_INCLUDED
+#include "position_echecs.h"
 
+
+int algo_minmax(position_echecs &P, int profondeur, int alpha, int beta);
+int minmax(position_echecs &P, int profondeur, int alpha, int beta);
+int Min(int a, int b);
+int Max(int a, int b);
+
+
+#endif // MINMAX_H_INCLUDED
