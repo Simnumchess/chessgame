1bb415bb4d374803d89039b39e2d073f4b948825
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-06 04:16:27 +0100

    Update minmax_ab.h

--- minmax_ab.h
+++ minmax_ab.h
@@ -3,12 +3,10 @@
 #define MINMAX_AB_H_INCLUDED
 #include "position_echecs.h"
 
-// rajouter double alpha, double beta
 
-//minmax classique
 int minmax_ab(position_echecs &P, int profondeur, int alpha, int beta);
-int minmax_ab_min(position_echecs &P, int profondeur, int alpha, int beta);//min à l'interieur du minmax
-int minmax_ab_max(position_echecs &P, int profondeur, int alpha, int beta);//max à l'interieur du minmax
+int Min(int a, int b);
+int Max(int a, int b);
 
 
 #endif // MINMAX_AB_H_INCLUDED
