b786b2418288a8d16bcbcd23c00e4e1663715494
kaeria <inesguo93@gmail.com>
2015-03-07 11:34:43 +0100

    Update echiquier.cpp

--- echiquier.cpp
+++ echiquier.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-ehciquier::echiquier() //initialisation
+echiquier::echiquier() //initialisation
 {
     dim=8; //la dimension d'un echiquier
     echectab=new piece*[dim];
@@ -15,128 +15,117 @@ ehciquier::echiquier() //initialisation
         echectab[i]=new piece[dim];
         {
             for(int j=0;j<dim;j++)
-            piece PV;
-            PV.type_piece=Piecevide;
-            ehcectab[i][j]=PV;
+            {
+                piece PV;
+                PV.type_piece=Piecevide;
+                echectab[i][j]=PV;
+            }
+            
         }
     }
     for(int i=0;i<8;i++)
     {
         piece Pb;
         Pb.type_piece=Pion;
-        Pb.col=blanc;
+        Pb.color=blanc;
         echectab[1][i]=Pb;
 
         piece Pn;
         Pn.type_piece=Pion;
-        Pn.col=noir;
+        Pn.color=noir;
         echectab[6][i]=Pn;
 
         piece Tb;
         Tb.type_piece=Tour;
-        Tb.col=blanc;
-        echec[0][0]=Tb;
-        echec[0][7]=Tb;
+        Tb.color=blanc;
+        echectab[0][0]=Tb;
+        echectab[0][7]=Tb;
 
         piece Tn;
         Tn.type_piece=Tour;
-        Tn.col=noir;
-        echec[7][0]=Tn;
-        echec[7][7]=Tn;
+        Tn.color=noir;
+        echectab[7][0]=Tn;
+        echectab[7][7]=Tn;
 
         piece Cb;
         Cb.type_piece=Cavalier;
-        Cb.col=blanc;
-        echec[0][1]=Cb;
-        echec[0][6]=Cb;
+        Cb.color=blanc;
+        echectab[0][1]=Cb;
+        echectab[0][6]=Cb;
 
         piece Cn;
-        Cn.type_piece=Cvalier;
-        Cn.col=noir;
-        echec[7][1]=noir;
-        echec[7][6]=noir;
+        Cn.type_piece=Cavalier;
+        Cn.color=noir;
+        echectab[7][1].color=noir;
+        echectab[7][6].color=noir;
 
         piece Fb;
         Fb.type_piece=Fou;
-        Fb.col=blanc;
-        echec[0][2]=Fb;
-        echec[0][5]=Fb;
+        Fb.color=blanc;
+        echectab[0][2]=Fb;
+        echectab[0][5]=Fb;
 
         piece Fn;
         Fn.type_piece=Fou;
-        Fn.col=noir;
-        echec[7][2]=Fn;
-        echec[7][5]=Fn;
+        Fn.color=noir;
+        echectab[7][2]=Fn;
+        echectab[7][5]=Fn;
 
         piece Db;
         Db.type_piece=Dame;
-        Db.col=blanc;
-        echec[0][3]=Db;
+        Db.color=blanc;
+        echectab[0][3]=Db;
 
         piece Dn;
         Dn.type_piece=Dame;
-        Dn.col=noir;
-        echec[7][3]=Dn;
+        Dn.color=noir;
+        echectab[7][3]=Dn;
 
         piece Rb;
         Rb.type_piece=Roi;
-        Rb.col=blanc;
-        echec[0][4]=Rb;
+        Rb.color=blanc;
+        echectab[0][4]=Rb;
 
         piece Rn;
         Rn.type_piece=Roi;
-        Rn.col=noir;
-        echec[7][4]=Dn;
+        Rn.color=noir;
+        echectab[7][4]=Dn;
     }
 
 }
 echiquier::~echiquier()
 {
     if(echectab!=NULL)
-    for(int i=0:i<8;i++)
+        for(int i=0;i<8;i++)
     delete []echectab[i];
     delete []echectab;
 }
 void echiquier::print()
 {
-    char* echec_tab_indicel[8];
-    char* ehcec_tab_indicec[8];
-    echec_tab_indicel[0]='A';
-    echec_tab_indicel[1]='B';
-    echec_tab_indicel[2]='C';
-    echec_tab_indicel[3]='D';
-    echec_tab_indicel[4]='E';
-    echec_tab_indicel[5]='F';
-    echec_tab_indicel[6]='G';
-    echec_tab_indicel[7]='H';
-    
-    echec_tab_indicec[0]='1';
-    echec_tab_indicec[1]='2';
-    echec_tab_indicec[2]='3';
-    echec_tab_indicec[3]='4';
-    echec_tab_indicec[4]='5';
-    echec_tab_indicec[5]='6';
-    echec_tab_indicec[6]='7';
-    echec_tab_indicec[7]='8';
-    for(int i=0:i<dim;i++)
+    char a[8]={'A','B','C','D','E','F','G','H'};
+    int b[8]={'1','2','3','4','5','6','7','8'};
+    for(int i=0;i<dim;i++)
     {
         cout<<"  ";
-        cout<<echec_tab_indicel[k]<<"  ";
+        cout<<a[i]<<"  ";
         cout<<endl;
     }
+    for(int i=0;i<dim;i++)
     {
-        for(int i=0;i<dim,i++)
+        cout<<b[i]<<" ";
+        for(int j=0;j<8;j++)
         {
-            cout<<echec_tab_indicec[i]<<" ";
-            for(int j=0;j<8;j++)
-            {
-                Piece P;
-                P=echectab[i][j];
-                P.print(); 
-                
-            }
-            cout<<echec_tab_indice[i]<<endl;
+            piece P;
+            P=echectab[i][j];
+            P.print();
         }
+        cout<<b[i]<<endl;
+    }
+    for(int i=0;i<dim;i++)
+    {
+        cout<<"  ";
+        cout<<a[i]<<"  ";
+        cout<<endl;
     }
 
 }
@@ -146,12 +135,12 @@ echiquier::echiquier(const echiquier & Echec) //constructeur par copie
     echectab=new piece*[dim];
     for(int i=0;i<dim;i++)
     {
-        for(j=0;j<dim;j++)
-        ehcectab[i][j]=Echec.echectab[i][j];
+        for(int j=0;j<dim;j++)
+        echectab[i][j]=Echec.echectab[i][j];
     }
 
 }
-echiquier::echiquier & operator=(const echiquier & Echec) //operator par copie
+echiquier & echiquier::operator=(const echiquier & Echec)  //operator par copie
 {
     dim=Echec.dim;
     echectab=NULL;
@@ -161,7 +150,7 @@ echiquier::echiquier & operator=(const echiquier & Echec) //operator par copie
      for(int i=0;i<dim;i++)
     {
         for(int j=0;j<dim;j++)
-        ehcectab[i][j]=Echec.echectab[i][j];
+        echectab[i][j]=Echec.echectab[i][j];
     }
     return *this;
 
