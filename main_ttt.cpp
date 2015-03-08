36df2271446963a9e0d682a9c51cd921d44eba37
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-08 20:33:50 +0100

    Update main_ttt.cpp

--- main_ttt.cpp
+++ main_ttt.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+#include <cmath>
+#include "ttt.h"
+#include "definition.h"
+
+using namespace std;
+
+
+int main()
+{
+//TicTacToe
+cout<<"Bienvenue dans notre TicTacToe !"<<endl;
+ttt TTT;
+TTT.print();
+cout<<"C'est à vous de jouer ! Vous êtes les croix."<<endl;
+cout<<"Où voulez-vous placer votre croix ?"<<endl;
+char placement_croix[2];
+cin>>placement_croix;
+cout<<"Vous avez choisi de placer votre croix en "<<placement_croix<<endl;
+
+
 
