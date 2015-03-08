9aa551683041e70ad97377a19d14e8f20c5fe670
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-08 13:55:17 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,7 +15,26 @@
 using namespace std
 
 //TicTacToe
+cout<<"Bienvenue dans notre TicTacToe"<<endl;
+ttt TTT;
+TTT.print();
+cout<<"C'est à vous de jouer ! Vous êtes les croix"<<endl;
+cout<<"Où voulez vous placer votre croix ?"<<endl;
+char placement_croix[2];
+cin>>placement_croix;
+
 
 
 
 //Echecs
+cout<<"Bienvenue dans notre jeu d'Echecs"<<endl;
+echiquier E;
+E.print();
+cout<<"C'est à vous de jouer !"<<endl;
+cout<<"Quel coup voulez-vous jouer ?"<<endl;
+//à modifier
+coup coup_choisi;
+cin>>coup;
+
+
+
