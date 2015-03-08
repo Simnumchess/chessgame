ae9416a27f4c8711b53e69b6e4cea765adb1a4ce
Marcow12 <marcantoinew12@gmail.com>
2015-03-08 19:12:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <ostream>
 #include "coup.h"
-#include "minmax_ab.h"
-#include "minmax.h"
+//#include "minmax_ab.h"
+//#include "minmax.h"
 #include "echiquier.h"
 #include "piece.h"
-#include "ttt.h"
-#include "position.h"
-#include "position_ttt.h"
-#include "position_echecs.h"
-#include "minmax_ttt.h"
-#include "definition.h"
+//#include "ttt.h"
+//#include "position.h"
+//#include "position_ttt.h"
+//#include "position_echecs.h"
+//#include "minmax_ttt.h"
+//#include "definition.h"
 
-using namespace std
+using namespace std;
 
+int main()
+{
+    echiquier e;
+    e.print();
+    cout<<endl;
+    piece P;
+    P.type_piece=Fou;
+    cout<<P.getvaleurpiece();
+    cout<<endl;
+    return 0;
+}
+
+/*
 //TicTacToe
 cout<<"Bienvenue dans notre TicTacToe"<<endl;
 ttt TTT;
@@ -35,6 +48,9 @@ cout<<"Quel coup voulez-vous jouer ?"<<endl;
 //à modifier
 coup coup_choisi;
 cin>>coup;
+*/
+
+
 
 
 
