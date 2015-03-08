130ac5b3347a1778425211f506c7c992579d7814
kaeria <inesguo93@gmail.com>
2015-03-08 12:11:58 +0100

    Update coup.h

--- coup.h
+++ coup.h
@@ -1,24 +1,31 @@
 #ifndef COUP_H_INCLUDED
 #define COUP_H_INCLUDED
-#include "piece.h"
-#include "echiquier.h"
-class coup
-{
-    public:
-    
-    couleur col;
-    piece piece_jouee;
-    piece piece_mangee;
-    
-    bool coup_possible(echiquier &); //explorer les coups possible en base des placements possibles
-    coup(); //initialisation d'un coup;
-    
-    position pos_ancienne; //l'ancienne position
-    position pos_nouvelle; //la nouvelle position
-    bool prise_de_piece(); //tester s'il y a prise de piece(un est mangee par l'autre)
-    enum coup_speciaux; //tout les coup speciaux
-    bool coup_special(); //tester si c'est un coup special
 
+#include "Piece.h"
+#include "Echiquier.h"
+
+//enum coup_speciaux{};
+
+class Coup
+{
+    public :
+
+    couleur color;
+    Piece piece_jouee;
+    int ancienne_l;//ancienne position ligne
+    int ancienne_c;//ancienne position colonne
+    int nouvelle_l;//nouvelle position ligne
+    int nouvelle_c;//nouvelle position colonne
+    bool prise_piece(Echiquier e);
+    //bool coup_special;
+    void conv_num_lettre(int n);//affiche la lettre correspondant au numero de colonne
+    void coda_std_cp(Echiquier e);
+
+    Coup(); //constructeur
+    Coup & operator=(const Coup &); //operateur = pour un coup
+     Coup(const Coup & p); //operateur par copie
+    ~Coup();
 };
 
 #endif // COUP_H_INCLUDED
+
