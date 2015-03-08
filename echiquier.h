4bd7e8c419ec273e4a16ea23ad52f0345ac62632
Ilana Lascar <ilana_lascar@hotmail.com>
2015-03-08 10:16:07 +0100

    Update echiquier.h

--- echiquier.h
+++ echiquier.h
@@ -10,7 +10,7 @@ class echiquier
     public:
     int dim;
     Piece **echectab; //le tableau 8*8 de pointeurs de pieces
-    bool casevide(int l,int c) //prend un case de coordonee (l,c) et ragarde si la case est vide
+    bool casevide(int l,int c) //prend un case de coordonee (l,c) et regarde si la case est vide
     void print(); //print un echiquier
     echiquier(); //initialisation d'un echiquier
     echiquier(const echiquier &); //operateur par copie
