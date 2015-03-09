9ad4fa40db451a11e2b74956378320bd24cda8f7
kaeria <inesguo93@gmail.com>
2015-03-09 17:13:32 +0100

    Update position_echecs.h

--- position_echecs.h
+++ position_echecs.h
@@ -10,24 +10,29 @@ class position_echecs
 {
     public:
     coup coup_joue; //
+    echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
+    
     // coup* coup_historique; //memorisation de l'historique des coups joues
     // coup annul_coup(); //annulation d'un coup??
-    int resultat(); //regarde si un joueur a gagne
-    int val_pos_ordinateur(); //renvoie la valeur de la position de l'ordinateur
-    int val_pos_humain(); //renvoir la valeur de la position du huamin
     position_echecs* pos_soeur; //un pointeur sur la position soeur
     position_echecs* pos_fille; //un pointeur sur la premiere position fille
     Joueur Jeu_pos; //un indicateur sur le joueur qui doit jouer cette position: {humain;ordinateur}
-    echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
-    position_echecs maj(); //mise a jour des positions
-    double getvaleur(); //renvoie la valeur d'une position
-    position_echecs* get_pos_suiv(); //constructeur de toutes les positions suivantes a une profondeur donnee
+    
+    echiquier maj(position_echecs ); //mise a jour des positions
+    double getvaleur(position_echecs ); //renvoie la valeur d'une position
+    position_echecs* get_pos_suiv(position_echecs ); //constructeur de toutes les positions suivantes a une profondeur donnee
     
     position_echecs(); //constructeur d'une position
     position_echecs(const position_echecs &); //constructeur par copie
     position_echecs & operator=(const position_echecs &); //operateur =
     ~position_echecs(); //destructeur d'une position
+    position_echecs& IA(position_echecs );
 };
+
+
+
+#endif // POSITION_ECHECS_H_INCLUDEDDED
+
 
 
 
