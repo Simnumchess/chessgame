6bbeef396c9193f90d7b9b7e987716d4e835daf1
kaeria <inesguo93@gmail.com>
2015-03-09 20:47:13 +0100

    Update position_echecs.h

--- position_echecs.h
+++ position_echecs.h
@@ -11,7 +11,7 @@ class position_echecs
     public:
     coup coup_joue; //
     echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
-    
+    int nbcoup(position_echecs );
     // coup* coup_historique; //memorisation de l'historique des coups joues
     // coup annul_coup(); //annulation d'un coup??
     position_echecs* pos_soeur; //un pointeur sur la position soeur
@@ -20,13 +20,15 @@ class position_echecs
     
     echiquier maj(position_echecs ); //mise a jour des positions
     double getvaleur(position_echecs ); //renvoie la valeur d'une position
-    position_echecs* get_pos_suiv(position_echecs ); //constructeur de toutes les positions suivantes a une profondeur donnee
+    position_echecs* get_pos_suiv(position_echecs & ); //constructeur de toutes les positions suivantes a une profondeur donnee
     
     position_echecs(); //constructeur d'une position
     position_echecs(const position_echecs &); //constructeur par copie
     position_echecs & operator=(const position_echecs &); //operateur =
     ~position_echecs(); //destructeur d'une position
-    void IA(position_echecs & );
+    void IA(position_echecs &);
+    
+    bool fin_partie(position_echecs );
 };
 
 
