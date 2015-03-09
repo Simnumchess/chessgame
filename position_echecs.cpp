b7764d292eea30e9758f8e708275b8dcf6470e93
Marcow12 <marcantoinew12@gmail.com>
2015-03-09 13:47:06 +0100

    Update position_echecs.cpp

--- position_echecs.cpp
+++ position_echecs.cpp
@@ -1,4 +1,3 @@
-
 #include "piece.h"
 #include "position_echecs.h"
 #include "echiquier.h"
@@ -65,7 +64,7 @@ double position_echecs::getvaleur()// la fonction qui renvoie la valeur d'une po
 }
 position_echecs* position_echecs::get_pos_suiv()
 {
-    
+
     position_echecs* pos_fille=new position_echecs[100]; //un tableau de position fille
     int n=0;
     if(Jeu_pos==humain) //si le humain joue
@@ -131,16 +130,16 @@ int position_echecs::resultat() const
     {
         if(coup_joue.piece_mangee.col==blanc)
         {
-            
+
             cout<<"You lose";
             return mini;
             else if()
             cout<<"You win";
             return maxi;
-            
+
         }
     }
-    
+
     else return 0;
     cout<<"Please continue...";
 }
@@ -158,7 +157,7 @@ position_echecs & position_echecs::explorer() //explorer 4 coups(2 par joueurs)
     cp.ligne_final=l2;
     cp.colone_final=c2;
     cp.col=echiquier_ref.echectab[l1][c1].col;
-    
+
 }
 */
 position_echecs position_echecs::maj() //mise a jour de l'echiquier de reference apres les coups jouees
@@ -172,9 +171,9 @@ position_echecs position_echecs::maj() //mise a jour de l'echiquier de reference
 
 position_echecs & position_echecs::operator=(const position_echecs & p) //operateur = pour une position_echec
 {
-    if(this==&p) 
+    if(this==&p)
     return *this;
-    else 
+    else
     {
         echiquier_ref=p.echiquier_ref;
         coup_joue=p.coup_joue;
@@ -193,9 +192,32 @@ position_echecs::position_echecs(const position_echecs & p) //operateur par copi
     pos_soeur=p.pos_soeur;
     return *this;
 }
-position_echecs::position_echecs(echiquier E) //constructeur de la position initiale
+//pos_fille= new pos(ech_ref,listecouppossible[0],profondeur-1)
+/*
+pos1=pos_fille;
+for (int i=1, i<nb_coup,i++)
+    pos1->pos_soeur
+
+pos1*=pos_fille
+pos1*=new
 {
-    echiquier_ref=E;
+    pos_fille->pos_soeur=new position(echiquier ref,c[1],profondeur-1);
+}
+
+pos_fille= new position()
+position* p=pos_fille;
+for i nb fille +11
+    p->pos_soeur=new position ()
+    p=p->pos_soeur;
+*/
+
+
+
+position_echecs::position_echecs(int profondeur,echiquier e,coup* coup ) //constructeur de la position initiale
+{
+    echiquier_ref=e;
+    if prof!=0
+    pos_fille= new position_echecs(profondeur-1,e)
 }
 position_ehcec & position_echec::IA()
 {
@@ -210,11 +232,11 @@ position_ehcec & position_echec::IA()
     cin>>l2;
     cout<<"colone=?"<<endl;
     cin>>c2;
-    
+
     coup cp=coup();
     cp.piece_jouee=echiquier_ref.echectab[l1][c1];
     cp.piece_mangee=echiquier_ref.echectab[l2][c2];
-    
+
     if(cp.coup_possible(echiquier_ref)==false)
     {
         cout<<"coup pas possible"<<endl;
@@ -233,7 +255,7 @@ position_ehcec & position_echec::IA()
     }
     maj();
     echiquier_ref.print();
-    
+
 }
 /*
 void partie() //definition d'une partie
@@ -241,15 +263,15 @@ void partie() //definition d'une partie
     echiquier E; //definition d'un echiquier
     E=echiquier(); //initiqlisqtion d'un echiquier
     E.print();
-    
+
     //coup possible pour le joueur humain
     //coup joue par le joueur huamin
-    
+
     //determiner le meilleur coup du joueur ordinateur(minmax)
     //coup joue par le joueur ordinateur
     position_echecs pos_init=position_echecs(E); //definition de la position de l'echiquier E
     pos_init.Jeu_pos=humain;
-    
-    
+
+
 }
 */
