78f91832cacdabe5e7de5dcd1ee506f9402e503a
Ilana Lascar <IlanaLascar@users.noreply.github.com>
2015-03-10 18:08:47 +0100

    Update piece.h

--- piece.h
+++ piece.h
@@ -14,7 +14,7 @@ class piece
     int deplacementc; //la posiiton d'une piece en colone apres un deplacement
     int i; //la position d'une piece en ligne
     int j; //la position d'une piece en colone
-    bool deplacement(); //regarde si on peut faire un deplacement
+    bool deplacement(int, int); //regarde si on peut faire un deplacement
     int val; //valeur d'une piece: val_roi=0; val_pion=v; val_reine=9*v; val_tour=5*v; val_fou=3*v; val_cavalier=3*v
     int getvaleurpiece(); //renvoie la valeur d'une piece selon son type
     piece(); //constructeur d'une piece
