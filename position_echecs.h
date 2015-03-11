bbb5d6ac315df6329cc89d5be73acf212c91125c
kaeria <inesguo93@gmail.com>
2015-03-11 14:39:39 +0100

    Update position_echecs.h

--- position_echecs.h
+++ position_echecs.h
@@ -1,4 +1,3 @@
-
 #ifndef POSITION_ECHECS_H_INCLUDED
 #define POSITION_ECHECS_H_INCLUDED
 #include "piece.h"
@@ -11,15 +10,15 @@ class position_echecs
     public:
     coup coup_joue; //
     echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
-    int nbcoup();
+    int nbcoup(position_echecs );
     // coup* coup_historique; //memorisation de l'historique des coups joues
     // coup annul_coup(); //annulation d'un coup??
     position_echecs* pos_soeur; //un pointeur sur la position soeur
     position_echecs* pos_fille; //un pointeur sur la premiere position fille
     Joueur Jeu_pos; //un indicateur sur le joueur qui doit jouer cette position: {humain;ordinateur}
     
-    echiquier maj(); //mise a jour des positions
-    int getvaleur(int ,int ); //renvoie la valeur d'une position
+    echiquier maj(position_echecs ); //mise a jour des positions
+    double getvaleur(position_echecs ); //renvoie la valeur d'une position
     position_echecs* get_pos_suiv(position_echecs & ); //constructeur de toutes les positions suivantes a une profondeur donnee
     
     position_echecs(); //constructeur d'une position
@@ -28,13 +27,8 @@ class position_echecs
     ~position_echecs(); //destructeur d'une position
     void IA(position_echecs &);
     
-    bool fin_partie();
+    bool fin_partie(position_echecs );
 };
-
-
-
-#endif // POSITION_ECHECS_H_INCLUDEDDED
-
 
 
 
