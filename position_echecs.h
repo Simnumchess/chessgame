92f563ac3e580babad7b64301ae40bc1c7f929ce
Ilana Lascar <IlanaLascar@users.noreply.github.com>
2015-03-12 02:24:22 +0100

    Update position_echecs.h

--- position_echecs.h
+++ position_echecs.h
@@ -1,3 +1,4 @@
+
 #ifndef POSITION_ECHECS_H_INCLUDED
 #define POSITION_ECHECS_H_INCLUDED
 #include "piece.h"
@@ -11,21 +12,25 @@ class position_echecs
     coup coup_joue; //
     echiquier echiquier_ref; //definition d'un echiquier de reference(le dernier etat connu de l'echiquier)
     int nbcoup();
+    // coup* coup_historique; //memorisation de l'historique des coups joues
+    // coup annul_coup(); //annulation d'un coup??
     position_echecs* pos_soeur; //un pointeur sur la position soeur
     position_echecs* pos_fille; //un pointeur sur la premiere position fille
-    Joueur Jeu_pos; //un indicateur sur le joueur qui doit jouer cette position: {humain;ordinateur}
+    Joueur Jeu_pos; //un indicateur sur le joueur qui doit jouer cette position: {humain, ordinateur}
     
     echiquier maj(); //mise a jour des positions
-    double getvaleur(); //renvoie la valeur d'une position
+    int getvaleur(); //renvoie la valeur d'une position
     position_echecs* get_pos_suiv(position_echecs & ); //constructeur de toutes les positions suivantes a une profondeur donnee
     
     position_echecs(); //constructeur d'une position
     position_echecs(const position_echecs &); //constructeur par copie
     position_echecs & operator=(const position_echecs &); //operateur =
     ~position_echecs(); //destructeur d'une position
-    void IA(position_echecs &);
     
-    bool fin_partie();
+    position_echecs IA();//jeu
+    
+    bool fin_partie();//indicateur de fin de partie
+    
 };
 
 
