6bd6831b292e1e4ab0a3ffa016b6fde6bcdf0ddf
Ilana Lascar <IlanaLascar@users.noreply.github.com>
2015-03-12 01:41:07 +0100

    Update minmax.cpp

--- minmax.cpp
+++ minmax.cpp
@@ -1,112 +1,297 @@
+#include "definition.h"
+#include "piece.h"
+#include <iostream>
+#include <ostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
 
-#include "minmax.h"
-#include "position_echecs.h"
+using namespace std;
 
-
-/*fonction ALPHABETA(P, alpha, beta) / alpha est toujours inférieur à beta 
-   si P est une feuille alors
-       retourner la valeur de P
-   sinon
-       si P est un nœud Min alors
-           Val = infini
-           pour tout enfant Pi de P faire
-               Val = Min(Val, ALPHABETA(Pi, alpha, beta))                
-               si alpha ≥ Val alors  / coupure alpha /
-                   retourner Val
-               finsi    
-               beta = Min(beta, Val)
-           finpour            
-       sinon
-           Val = -infini
-           pour tout enfant Pi de P faire
-               Val = Max(Val, ALPHABETA(Pi, alpha, beta))                
-               si Val ≥ beta alors / coupure beta /
-                   retourner Val
-               finsi   
-               alpha = Max(alpha, Val)
-           finpour
-       finsi
-   retourner Val
-   finsi
-fin
-*/
-
-
-
-int algo_minmax (position_echecs &P, int profondeur, int a, int b){
-   
-   //On génere les positions filles (positions que peut jouer l'ordinateur)
-   position_echecs *F=P.get_pos_suiv(P);
-   int a=P.nbcoup(P);
-   cout <<"nombre de possibilités pour l'ordinateur : "<<a<<endl;
-   int max=-1000;
-   int indice_position_fille=0;
-
-//C'est à l'ordinateur de jouer donc on prend le max des positions filles (positions que peut jouer l'ordinateur)
-   for(int i=0; i<a;i++){
-         int val=minmax(F[i],profondeur,alpha,beta, a,b);
-         if(val>max){
-            max=val;
-            indice_position_fille=i;
-         }
-   }
-   
-   return indice_position_fille;
-   //maintenant il faut jouer cette position fille !
-}
-
-
-
-int minmax(position_echecs &P, int profondeur, int a, int b){
-
-cout<<"minmax classique"<<endl;
-
-if(profondeur<0) {cout<<"probleme avec la profondeur"<<endl;}
-
-
-if (profondeur==0) return P.getvaleur(P, a, b);
-
-//On génere les positions filles
-position_echecs *F=P.get_pos_suiv(P)
-int a=P.nbcoup(P);
-
-
-if (P.Jeu_pos==fin_partie) return P.getvaleur(P, a, b);
-
-else if(P.Jeu_pos==ordinateur)
-{ 
-  int max=-1000;
-  for(int i=0;i<a;i++)// on parcourt les positions filles
-  {
-      max=Max(max,minmax(F[i], profondeur-1, a, b));
-  }
-}
-else if (P.Jeu_pos==humain)
+void piece::print()
 {
-   int min=1000;
-  for(int i=0;i<a;i++)// on parcourt les positions filles
-  {
-      min=Min(min,minmax_ab(F[i], profondeur-1, a, b));
-  }
+    switch(type_piece)
+    {
+        case Pion:
+        if(color==blanc)
+        cout<<"Pb"<<" ";
+        else cout<<"Pn"<<" ";
+        break;
+
+        case Tour:
+        if(color==blanc)
+        cout<<"Tb"<<" ";
+        else cout<<"Tn"<<" ";
+        break;
+
+        case Cavalier:
+        if(color==blanc)
+        cout<<"Cb"<<" ";
+        else cout<<"Cn"<<" ";
+        break;
+
+        case Fou:
+        if(color==blanc)
+        cout<<"Fb"<<" ";
+        else cout<<"Fn"<<" ";
+        break;
+
+        case Dame:
+        if(color==blanc)
+        cout<<"Db"<<" ";
+        else cout<<"Dn"<<" ";
+        break;
+
+        case Roi:
+        if(color==blanc)
+        cout<<"Rb"<<" ";
+        else cout<<"Rn"<<" ";
+        break;
+
+        case Piecevide:
+        cout<<"   ";//3espaces 2 pour les lettres et un espace
+        break;
+    }
+}
+int piece::getvaleurpiece()
+{
+    int v=1; //on decide la valeur du v
+    switch(type_piece)
+    {
+        case Pion:
+        return v;
+        break;
+        case Tour:
+        return 5*v;
+        break;
+        case Cavalier:
+        return 3*v;
+        break;
+        case Fou:
+        return 3*v;
+        break;
+        case Dame:
+        return 9*v;
+        break;
+        case Roi:
+        return 0;
+        break;
+        case Piecevide:
+        return 0;
+        break;
+    }
+}
+bool piece::deplacement(int l,int c)
+{
+    piece Piece;
+    l=Piece.deplacementl;
+    c=Piece.deplacementc; //(l,c) coordonnee de la piece apres deplacement
+    int i=Piece.i;
+    int j=Piece.j; //(i,l) coordonnee de la piece apres deplacement
+    switch(type_piece)
+    {
+        case Pion:
+        {
+            if(color==noir)
+            {
+                if( ( ((i-1)==l)&&(j==c) )||( ((i-2)==l)&&(j==c)&&(i==6) ) || ( ((i-1)==l)&&(j-1)==c) || ( ((i-1)==l)&&(j+1)==c))
+                {
+                    return true;
+                    break;
+                }
+                else
+                {
+                    return false;
+                    break;
+                }
+            }
+            else if(color==blanc)
+            {
+                if((((i+1)==l)&&(j==c))||(((i+2)==l)&&(j==c)&&(i==1)))
+                {
+                    return true;
+                    break;
+                }
+                else
+                {
+                    return false;
+                    break;
+                }
+
+            }
+        }
+        case Tour:
+        {
+            if(j==c)
+            {
+                return true;
+                break;
+            }
+            else
+            {
+                if(i==l)
+                {
+                    return true;
+                    break;
+                }
+                else
+                {
+                    return false;
+                    break;
+                }
+            }
+        }
+        case Cavalier:
+        {
+            if((i==(l+1))&&(j==(c+2)))
+            {
+                return true;
+                break;
+            }
+            else if((i==(l+2))&&(j==(c+1)))
+            {
+                return true;
+                break;
+            }
+            else if((i==(l-1))&&(j==(c+2)))
+            {
+                return true;
+                break;
+            }
+            else if((i==(l-2))&&(j==(c+1)))
+            {
+                return true;
+                break;
+            }
+            else if((i==(l+2))&&(j==(c-1)))
+            {
+                return true;
+                break;
+            }
+            else if((i==(l+1))&&(j==(c-2)))
+            {
+                return true;
+                break;
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        case Fou:
+        {
+            if(((i-l)==(j-c))||((i-l)==(c-j)))
+            {
+                return true;
+                break;
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        case Dame:
+        {
+            if(abs(i-l)==abs(j-c))
+            {
+                return true;
+                break;
+            }
+            if(j==c)
+            {
+                return true;
+                break;
+            }
+            else
+            {
+                if(i==l)
+                {
+                    return true;
+                    break;
+                }
+                else
+                {
+                    return false;
+                    break;
+                }
+            }
+        }
+        case Roi:
+        {
+            if (i==l+1 && j==c)
+            {
+                return true;
+                break;
+            }
+
+            else if (i==l && j==c+1)
+            {
+                return true;
+                break;
+            }
+            else if (i==l-1 && j==c+1)
+            {
+                return true;
+                break;
+            }
+            else if (i==l+1 && j==c+1)
+            {
+                return true;
+                break;
+            }
+            else if (i==l && j==c)
+            {
+                return true;
+                break;
+            }
+            else if (i==l-1 && j==c)
+            {
+                return true;
+                break;
+            }
+            else
+            {
+                return false;
+                break;
+            }
+        }
+        case Piecevide:
+        {
+            return false;
+            break;
+        }
+    }
+}
+piece& piece::operator=(const piece & P)
+{
+        if (this==&P) return *this;
+        else
+        {
+            type_piece=P.type_piece;
+            color=P.color;
+            val=P.val;
+            i=P.i;
+            j=P.j;
+            deplacementl=P.deplacementl;
+            deplacementc=P.deplacementc;
+            return *this;
+        }
 }
 
+piece::piece()
+    {
+        type_piece=Piecevide;
+        val=0;
+    }
+
+piece::~piece()
+{
 }
 
 
 
-int Min(int a, int b){
-  int min=0;
-  if (a>b) min=b;
-  else min=a;
-  return min;
-}
 
 
-
-int Max(int a, int b){
-   int max=0;
-   if(a>b) max=a;
-   else max=b;
-  return max;
-}
 
