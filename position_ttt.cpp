a76016dfc33b4a2daf2351f488bcbb71064a165a
Ilana Lascar <IlanaLascar@users.noreply.github.com>
2015-03-12 02:24:44 +0100

    Update position_ttt.cpp

--- position_ttt.cpp
+++ position_ttt.cpp
@@ -1,162 +1,182 @@
-#include "position.h"
-#include "piece.h"
-#include "position_ttt.h"
+
+ #include "position_ttt.h"
 #include "ttt.h"
-#include "coup.h"
-#include "minmax_ttt.h"
+#include "definition.h"
 #include <ostream>
 #include <iostream>
+
 using namespace std;
 
-position_ttt::~position_ttt() // destructeur de toutes les positions filles
+position_ttt::position_ttt() //constructeur de la position initiale
 {
-    if((pos_fille!=NULL)||(pos_soeur!==NULL))
-    delete []pos_fille;
-    delete []pos_soeur;
+    ttt_ref=ttt();
+    pos_fille=NULL;
+    pos_soeur=NULL;
 }
 
 
-
-position_ttt* position_ttt::get_pos_suiv()
+position_ttt::~position_ttt()// destructeur de toutes les positions filles
 {
-    position_ttt* pos_fille=new position_ttt[100]; //un tableau de position fille
-    int n=0;
-    if(Jeu_pos==humain) //si le humain joue
+    if((pos_fille!=NULL)||(pos_soeur!=NULL))
     {
-        for(int l=0;l<8;l++)
-        {
-            for(int c=0;c<8;c++)
-            {
-                if((ttt_ref.ttttab[l][c]!=0)&&(echiquier_ref.ehectab[l][c].col==blanc))
-                //coup possible??
-                //class coup: piece_jouee;piece_mangee;coordonnee avant le coup;coordonnee apres le coup;bool coup_possible()
-                coup C;
-                C.piece_jouee=ttt_ref.ttttab[l][c];
-                if(bool coup_possible(coup C)==true)
-                {
-                    pos_fille[n]=position_ttt(*this);
-                    if(Jeu_pos==humain)
-                    {
-                        pos_fille[n].Jeu_pos==ordinateur;
-                    }
-                    else
-                    {
-                        pos_fille[n].Jeu_pos==humain;
-                    }
-                    n++;
-                }
-                
-                
-                
-            }
-        }
-        return pos_fille;
-    }
-    else if(Jeu_pos==ordinateur) //si l'ordinateur joue
-    {
-        for(int l=0;l<8;l++)
-        {
-            for(int c=0;c<8;c++)
-            {
-                if(ttttab[l][c]!=0)
-                //coup possible??
-                
-            }
-        }
-        return pos_fille;
+        delete []pos_fille;
+        delete []pos_soeur;
     }
 }
 
 
+ttt position_ttt::maj(int i) //mise a jour de l'echiquier de reference apres les coups joués
+{
+    if(getJoueur()=="humain") ttt_ref.ttttab[i]='X';
+    else ttt_ref.ttttab[i]='O';
+    return ttt_ref;
 
-int position_ttt::resultat() const
-{// à refaire pour le ttt
-    if(coup_joue.piece_mangee.type_piece==Roi)
-    {
-        if(coup_joue.piece_mangee.col==blanc)
-        {
-            
-            cout<<"You lose";
-            return mini;
-            else 
-            cout<<"You win";
-            return maxi;
-            
+}
+
+
+position_ttt* position_ttt::get_pos_suiv(position_ttt &p)
+{
+    ttt E=p.ttt_ref;
+    int nbcoup=0;
+    for (int i=0;i<10;i++) {
+        if (E.casevide(i)==true) {
+            nbcoup++;
         }
     }
-    
-    else return 0;
-    cout<<"Please continue..."
+    position_ttt* p_current = & p;
+    p.pos_fille = &p;
+    for(int i=0;i<nbcoup;i++)
+    {
+        
+        p.pos_fille->ttt_ref=maj(i);
+        p.pos_fille->pos_fille= get_pos_suiv(*p.pos_fille);
+        p_current->pos_soeur = p.pos_fille;
+        p_current = p.pos_fille;
+    }
+    return  p.pos_fille;
 }
 
 
-position_ttt & position_ttt::explorer() //explorer 4 coups(2 par joueurs)
+position_ttt & position_ttt::operator=(const position_ttt & p) //operateur = pour une position_echec
 {
-    int l1,l2,c1,c2=0;
-    //explorer les coups
-    coup cp=coup();
-    cp.piece_jouee=ttt_ref[l1][c1];
-    cp.ligne_init=l1;
-    cp.colone_init=c1;
-    cp.ligne_final=l2;
-    cp.colone_final=c2;
-    cp.col=ttt_ref.ttttab[l1][c1].col;
-    
-}
-
-
-position_ttt position_ttt::maj() //mise a jour de l'echiquier de reference apres les coups jouees
-{
-    //coup possible
-}
-
-
-
-position_ttt & position_ttt::operator=(const position_ttt & p) //operateur = pour une position_ttt
-{
-    if(this==&p) 
-    return *this;
-    else 
+    if(this==&p)
+        return *this;
+    else
     {
         ttt_ref=p.ttt_ref;
-        coup_joue=p.coup_joue;
-        coup_historique=p.coup_historique;
         pos_fille=p.pos_fille;
         pos_soeur=p.pos_soeur;
         return *this;
     }
 }
+
 position_ttt::position_ttt(const position_ttt & p) //operateur par copie
 {
     ttt_ref=p.ttt_ref;
-    coup_joue=p.coup_joue;
-    coup_historique=p.coup_historique;
     pos_fille=p.pos_fille;
     pos_soeur=p.pos_soeur;
-    return *this;
+}
+
+position_ttt position_ttt::IA()
+{
+    position_ttt P;
+    P.ttt_ref.print();
+    int c=0;
+    cout<<"Choisissez la case :"<<endl;
+    cout<<"Il faut entrer des chiffres."<<endl;
+    cin>>c;
+    cout<<endl;
+    
+    if(ttt_ref.casevide(c)==false)
+    {
+        cout<<"Coup interdit. Il faut choisir un autre coup..."<<endl;
+        IA();
+    }
+    else
+    {
+        P.ttt_ref.ttttab[c]='X';
+    }
+    P.ttt_ref.print();
+    
+    return P;
 }
 
 
-position_ttt::position_ttt(ttt E) //constructeur de la position initiale
+
+char position_ttt::fin_partie()
 {
-    ttt_ref=E;
+    ttt T=ttt_ref;
+    
+    if (T.nb_casesvides()==0)
+    {
+        
+        cout<<"Fin de la partie !"<<endl;
+        
+        
+        if(T.ttttab[1]==T.ttttab[2]) {
+            if(T.ttttab[2]==T.ttttab[3])
+                cout<<T.ttttab[1]<<" a gagné !"<<endl;
+            return T.ttttab[1];
+        }
+        
+        if(T.ttttab[4]==T.ttttab[5]) {
+            if(T.ttttab[5]==T.ttttab[6])
+                cout<<T.ttttab[4]<<" a gagné !"<<endl;
+            return T.ttttab[4];
+        }
+        
+        if(T.ttttab[7]==T.ttttab[8]) {
+            if(T.ttttab[8]==T.ttttab[9])
+                cout<<T.ttttab[7]<<" a gagné !"<<endl;
+            return T.ttttab[7];
+        }
+        
+        if(T.ttttab[1]==T.ttttab[4]) {
+            if(T.ttttab[4]==T.ttttab[7])
+                cout<<T.ttttab[1]<<" a gagné !"<<endl;
+            return T.ttttab[1];
+        }
+        
+        if(T.ttttab[2]==T.ttttab[5]) {
+            if(T.ttttab[5]==T.ttttab[8])
+                cout<<T.ttttab[2]<<" a gagné !"<<endl;
+            return T.ttttab[2];
+        }
+        
+        if(T.ttttab[3]==T.ttttab[6]) {
+            if(T.ttttab[6]==T.ttttab[9])
+                cout<<T.ttttab[3]<<" a gagné !"<<endl;
+            return T.ttttab[3];
+        }
+        
+        if(T.ttttab[1]==T.ttttab[5]) {
+            if(T.ttttab[5]==T.ttttab[9])
+                cout<<T.ttttab[1]<<" a gagné !"<<endl;
+            return T.ttttab[1];
+        }
+        
+        if(T.ttttab[3]==T.ttttab[5]) {
+            if(T.ttttab[5]==T.ttttab[7])
+                cout<<T.ttttab[7]<<" a gagné !"<<endl;
+            return T.ttttab[3];
+        }
+        
+        else cout<<"Match nul...Retente ta chance !"<<endl;
+        
+        
+    }
+    
+    else return '0';
+    return 0;
+
 }
 
 
-void partie() //definition d'une partie
-{
-    ttt TTT; //definition d'un ttt
-    TTT=ttt(); //initiqlisqtion d'un ttt
-    TTT.print();
-    
-    //coup possible pour le joueur humain
-    //coup joue par le joueur huamin
-    
-    //determiner le meilleur coup du joueur ordinateur(minmax)
-    //coup joue par le joueur ordinateur
-    position_ttt pos_init=position_ttt(TTT); //definition de la position du ttt TTT
-    pos_init.Jeu_pos=humain;
-    
-    
+string position_ttt::getJoueur(){
+    string joueur;
+    int nb=ttt_ref.nb_casesvides();
+    if (nb==9||nb==7||nb==5||nb==3||nb==1) {joueur=humain;}
+    else {joueur=ordinateur;}
+    return joueur;
 }
 
