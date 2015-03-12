7b43a952ce3fd8ef644fb3af7581a516c1af766b
Ilana Lascar <IlanaLascar@users.noreply.github.com>
2015-03-12 02:21:56 +0100

    Update minmax_ttt.h

--- minmax_ttt.h
+++ minmax_ttt.h
@@ -1,9 +1,10 @@
-#ifndef MINMAX_H_INCLUDED
-#define MINMAX_H_INCLUDED
+#ifndef MINMAX_TTT_H_INCLUDED
+#define MINMAX_TTT_H_INCLUDED
+#include "position_ttt.h"
 
 
-int algo_minmax(ttt &TTT, int profondeur, int alpha, int beta);
-int minmax_ttt(ttt &TTT, int profondeur, int alpha, int beta);
+int algo_minmax_ttt(position_ttt &P);
+int minmax_ttt(position_ttt &P, int alpha, int beta);
 int Min(int a, int b);
 int Max(int a, int b);
 
